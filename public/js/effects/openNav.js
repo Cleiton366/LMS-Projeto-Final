73071b927f154f2b9b47e0eec0f1bee00c67513f
viktorsoares <js709406@gmail.com>
2023-11-30 21:55:50 -0300

    Ajusta nav js, scroll, slider e darkmode

---
+++ public/js/effects/openNav.js
@@ -0,0 +1,9 @@
+/* side menu */
+
+function openNav() {
+    document.getElementById("sideNav").style.width = "18rem";
+}
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+}
+
